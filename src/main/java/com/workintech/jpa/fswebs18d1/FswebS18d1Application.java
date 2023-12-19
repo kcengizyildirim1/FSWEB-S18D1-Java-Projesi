381bf913fe299c1784217e30a3068a08d2e80278
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2023-12-20 01:32:58 +0300

    init project

---
+++ src/main/java/com/workintech/jpa/fswebs18d1/FswebS18d1Application.java
@@ -0,0 +1,13 @@
+package com.workintech.jpa.fswebs18d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FswebS18d1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FswebS18d1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/jpa/fswebs18d1/FswebS18d1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.jpa.fswebs18d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FswebS18d1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
